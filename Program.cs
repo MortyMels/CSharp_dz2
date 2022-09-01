725ff56228df793f2e4b4463bdeb94bee186f5c1
MortyMels <Morty.Mels@gmail.com>
2022-09-01 13:42:09 +0300

    dz02.001.01

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool r = true;
+while(r){
+    Console.WriteLine("Введите несколько наборов из трех чисел в формате '0.0, 0.0, 0.0;'");
+
+    string s = Console.ReadLine().Replace(" ", "");
+    string[] sa = s.Split(';'); //разбили на точки
+    double[,] cl = new double[2,3];
+    object[] clc = {};
+    bool fi = true;
+    foreach (var san in sa)
+    {
+        string[] sani = san.Split(','); //x, y, z
+
+        try{ //проверка на корректность ввода координат 
+            if(sani.Length==3){
+                bool f = true;
+
+                int i = 0;
+                
+                foreach (var sanin in sani)
+                {
+                    try { //проверка на корректность ввода числа
+                        double saninb = double.Parse(sanin);
+                        cl[1,i++]=saninb;
+                    } catch {
+                        f=false;
+                        Console.WriteLine($"Нераспознаный символ '{sanin}' координат: {String.Join(", ", sani)}");
+                    }
+                }
+                if(!fi&f){
+                    Console.WriteLine($"Расстояние между точками ({cl[0,0]},{cl[0,1]},{cl[0,2]}) и ({cl[1,0]},{cl[1,1]},{cl[1,2]}) = { Math.Sqrt(cl[0,0]*cl[1,0]+cl[0,1]*cl[1,1]+cl[0,2]*cl[1,2])}");
+                }
+                if(f){
+                    fi=false;
+                    cl[0,0]=cl[1,0];
+                    cl[0,1]=cl[1,1];
+                    cl[0,2]=cl[1,2];
+                }
+            }else{
+                Console.WriteLine($"Неверное количество параметров в координатах: {san}");
+            }
+        } catch {
+            Console.WriteLine($"Координаты введены некорректно: {san}");
+        }
+    }
+    r=false;
+}
